fb1c7a852fb1a0fbb16a50278019434e3d70d0f2
Onofrei Constantin <constantin.onofrei22@gmail.com>
2024-11-18 21:23:14 +0200

    fixed strapi redirect problem

--- actions/stripe.ts
+++ actions/stripe.ts
@@ -3,57 +3,51 @@
 import { auth } from "@/auth";
 import prisma from "@/lib/db";
 import { headers } from "next/headers";
-import { notFound, redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 async function checkout(id: string) {
-  try {
-    const session = await auth();
+  const session = await auth();
 
-    if (!session?.user)
-      return { error: "You have no access to call this action!" };
+  if (!session?.user)
+    return { error: "You have no access to call this action!" };
 
-    const subscription = await prisma.subscriptions.findUnique({
-      where: { id },
-    });
+  const subscription = await prisma.subscriptions.findUnique({
+    where: { id },
+  });
 
-    if (!subscription) notFound();
+  if (!subscription) return { error: "The subscription no longer exists!" };
 
-    const headersList = await headers();
-    const origin = headersList.get("origin");
+  const headersList = await headers();
+  const origin = headersList.get("origin");
 
-    const paymentSession = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            currency: "RON",
-            product_data: {
-              name: subscription.title,
-            },
-            unit_amount: subscription.price * 100,
+  const paymentSession = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "RON",
+          product_data: {
+            name: subscription.title,
           },
-          quantity: 1,
+          unit_amount: subscription.price * 100,
         },
-      ],
-      payment_intent_data: {
-        metadata: { id, userId: String(session.user.id) },
+        quantity: 1,
       },
-      customer_email: String(session.user.email),
-      mode: "payment",
-      success_url: `${origin}/purchase/success`,
-      cancel_url: `${origin}/purchase/cancel`,
-    });
+    ],
+    payment_intent_data: {
+      metadata: { id, userId: String(session.user.id) },
+    },
+    customer_email: String(session.user.email),
+    mode: "payment",
+    success_url: `${origin}/purchase/success`,
+    cancel_url: `${origin}/purchase/cancel`,
+  });
 
-    if (!paymentSession.url) return { error: "No checkout page was provided!" };
+  if (!paymentSession.url) return { error: "No checkout page was provided!" };
 
-    redirect(paymentSession.url);
-  } catch (error) {
-    return {
-      error: error instanceof Error ? error.message : "Something went wrong!",
-    };
-  }
+  redirect(paymentSession.url);
 }
 
 export { checkout };
